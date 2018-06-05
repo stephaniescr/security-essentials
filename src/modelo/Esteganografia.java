1cebb220704573a7db3e5ae719ac030214e2c502
Stéphanie <stephanie.scremin@gmail.com>
2018-06-05 11:20:07 -0300

    Ajuste em comentários

--- src/modelo/Esteganografia.java
+++ src/modelo/Esteganografia.java
@@ -10,59 +10,58 @@ import javax.imageio.ImageIO;
 import javax.swing.JOptionPane;
 
 public class Esteganografia {
-    	
-	/*
-	 *Encrypt an image with text, the output file will be of type .png
-	 *@param path         The path (folder) containing the image to modify
-	 *@param original    The name of the image to modify
-	 *@param ext1          The extension type of the image to modify (jpg, png)
-	 *@param stegan      The output name of the file
-	 *@param message  The text to hide in the image
-	 *@param type	  integer representing either basic or advanced encoding
+
+	 /*
+	 Encripta uma imagem com texto, o resultado será .png
+	 @param path         O caminho da imagem a ser modificada
+	 @param original    O nome da imagem que será modificada
+	 @param ext1          A extensão da imagem a ser modificada (jpg, png)
+	 @param stegan      O output do nome do arquivo
+	 @param message  O texto a ser escrito na imagem
+	 @param type	  integer que representa encriptação simples ou avançada
 	 */
 	public static boolean encode(String path, String original, String ext1, String stegan, String message)
 	{
 		String			file_name 	= image_path(path,original,ext1);
 		BufferedImage 	image_orig	= getImage(file_name);
-		
-		//user space is not necessary for Encrypting
+
 		BufferedImage image = user_space(image_orig);
 		image = add_text(image,message);
-		
+
 		return(setImage(image,new File(image_path(path,stegan,"png")),"png"));
 	}
-	
-	/*
-	 *Decrypt assumes the image being used is of type .png, extracts the hidden text from an image
-	 *@param path   The path (folder) containing the image to extract the message from
-	 *@param name The name of the image to extract the message from
-	 *@param type integer representing either basic or advanced encoding
+
+	 /*
+   A decriptação assume que a imagem  da qual a mensagem será extraída é do tipo .png
+	 @param path   O path (folder) que contém a imagem da qual a mensagem será extraída
+	 @param name O nome da imagem da qual a mensagem será extraída
+	 @param type integer que representa encriptação simples ou avançada
 	 */
 	public static String decode(String path, String name)
 	{
 		byte[] decode;
 		try
 		{
-			//user space is necessary for decrypting
+			//user space é necessário para decriptação
 			BufferedImage image  = user_space(getImage(image_path(path,name,"png")));
 			decode = decode_text(get_byte_data(image));
 			return(new String(decode));
 		}
         catch(Exception e)
         {
-			JOptionPane.showMessageDialog(null, 
-				"There is no hidden message in this image!","Error",
+			JOptionPane.showMessageDialog(null,
+				"Não existe mensagem escondida nessa imagem","Erro",
 				JOptionPane.ERROR_MESSAGE);
 			return "";
         }
     }
-    
-	/*
-	 *Returns the complete path of a file, in the form: path\name.ext
-	 *@param path   The path (folder) of the file
-	 *@param name The name of the file
-	 *@param ext      The extension of the file
-	 *@return A String representing the complete path of a file
+
+	 /*
+	 Retorna o path completo de uma arquivo, da forma: path\nome.ext
+	 @param path   O path (folder) do arquivo
+	 @param name O nome do arquivo
+	 @param ext      A extensão do arquivo
+	 @return Uma String que representa o path completo do arquivo
 	 */
 	private static String image_path(String path, String name, String ext)
 	{
@@ -70,100 +69,100 @@ public class Esteganografia {
                 System.out.println(imagePath);
 		return imagePath;
 	}
-	
-	/*
-	 *Get method to return an image file
-	 *@param f The complete path name of the image.
-	 *@return A BufferedImage of the supplied file path
-	 *@see    Steganography.image_path
+
+	 /*
+	 Método get para retornar um aruqivo imagem
+	 @param f O path completo da imagem
+	 @return Uma BufferedImage do path fornecido
+	 @see    Steganography.image_path
 	 */
 	private static BufferedImage getImage(String f)
 	{
 		BufferedImage 	image	= null;
 		File 		file 	= new File(f);
-		
+
 		try
 		{
 			image = ImageIO.read(file);
 		}
 		catch(Exception ex)
 		{
-			JOptionPane.showMessageDialog(null, 
-				"Image could not be read!","Error",JOptionPane.ERROR_MESSAGE);
+			JOptionPane.showMessageDialog(null,
+				"A imagem não foi lida","Erro",JOptionPane.ERROR_MESSAGE);
 		}
 		return image;
 	}
-	
-	/*
-	 *Set method to save an image file
-	 *@param image The image file to save
-	 *@param file      File  to save the image to
-	 *@param ext      The extension and thus format of the file to be saved
-	 *@return Returns true if the save is succesful
+
+	 /*
+	 Método para salvar um arquivo de imagem
+	 @param image O arquivo imagem a ser salvo
+	 @param file      Arquivo no qual a imagem será salva
+	 @param ext      A extensão (formato) no qual será salva
+	 @return Retorna true se a imagem for salva
 	 */
 	private static boolean setImage(BufferedImage image, File file, String ext)
 	{
 		try
 		{
-			file.delete(); //delete resources used by the File
+			file.delete(); //deleta resources usados pelo arquivo
 			ImageIO.write(image,ext,file);
 			return true;
 		}
 		catch(Exception e)
 		{
-			JOptionPane.showMessageDialog(null, 
-				"File could not be saved!","Error",JOptionPane.ERROR_MESSAGE);
+			JOptionPane.showMessageDialog(null,
+				"O arquivo não foi salvo","Erro",JOptionPane.ERROR_MESSAGE);
 			return false;
 		}
 	}
-	
-	/*
-	 *Handles the addition of text into an image
-	 *@param image The image to add hidden text to
-	 *@param text     The text to hide in the image
-	 *@return Returns the image with the text embedded in it
+
+	 /*
+	 Adiciona texto a imagem
+	 @param image A imagem na qual será adicionada a imagem
+	 @param text     O texto a ser escondido na imagem
+	 @return Retorna uma imagem com o texto escondido nela
 	 */
 	private static BufferedImage add_text(BufferedImage image, String text)
 	{
-		//convert all items to byte arrays: image, message, message length
+		//converte todos os itens para byte arrays: image, message, message length
 		byte img[]  = get_byte_data(image);
 		byte msg[] = text.getBytes();
 		byte len[]   = bit_conversion(msg.length);
 		try
 		{
-			encode_text(img, len,  0); //0 first positiong
-			encode_text(img, msg, 32); //4 bytes of space for length: 4bytes*8bit = 32 bits
+			encode_text(img, len,  0);
+			encode_text(img, msg, 32);
 		}
 		catch(Exception e)
 		{
-			JOptionPane.showMessageDialog(null, 
-"Target File cannot hold message!", "Error",JOptionPane.ERROR_MESSAGE);
+			JOptionPane.showMessageDialog(null,
+"Arquivo de destino não suporta a mensagem", "Erro",JOptionPane.ERROR_MESSAGE);
 		}
 		return image;
 	}
-	
-	/*
-	 *Creates a user space version of a Buffered Image, for editing and saving bytes
-	 *@param image The image to put into user space, removes compression interferences
-	 *@return The user space version of the supplied image
+
+	 /*
+	 Cria uma versão user space da  BufferedImage, para editar e salvar bytes
+	 @param image A imagem a ser colocada no user space, remove interferências de compressão
+	 @return A versão user space da imagem enviada
 	 */
 	private static BufferedImage user_space(BufferedImage image)
 	{
-		//create new_img with the attributes of image
+		//cria new_img, com os atributos da imagem
 		BufferedImage new_img  = new BufferedImage(image.getWidth(), image.getHeight(), BufferedImage.TYPE_3BYTE_BGR);
 		Graphics2D    graphics = new_img.createGraphics();
 		graphics.drawRenderedImage(image, null);
-		graphics.dispose(); //release all allocated memory for this image
+		graphics.dispose(); //liberaa toda a memória alocada para essa imagem
 		return new_img;
 	}
-	
-	/*
-	 *Gets the byte array of an image
-	 *@param image The image to get byte data from
-	 *@return Returns the byte array of the image supplied
-	 *@see Raster
-	 *@see WritableRaster
-	 *@see DataBufferByte
+
+	 /*
+   Pega o byte array da imagem
+	 @param image A imagem da qual será extraído os dados em byte
+	 @return Retorna o byte array para a imagem enviada
+	 @see Raster
+	 @see WritableRaster
+	 @see DataBufferByte
 	 */
 	private static byte[] get_byte_data(BufferedImage image)
 	{
@@ -171,85 +170,80 @@ public class Esteganografia {
 		DataBufferByte buffer = (DataBufferByte)raster.getDataBuffer();
 		return buffer.getData();
 	}
-	
-	/*
-	 *Gernerates proper byte format of an integer
-	 *@param i The integer to convert
-	 *@return Returns a byte[4] array converting the supplied integer into bytes
+
+	 /*
+	 Converte integer para byte
+	 @param i A integer a ser convertida
+	 @return Retorna um array byte[4], convertendo a integer enviada para bytes
 	 */
 	private static byte[] bit_conversion(int i)
 	{
-		//originally integers (ints) cast into bytes
+		//integers (ints) para bytes
 		//byte byte7 = (byte)((i & 0xFF00000000000000L) >>> 56);
 		//byte byte6 = (byte)((i & 0x00FF000000000000L) >>> 48);
 		//byte byte5 = (byte)((i & 0x0000FF0000000000L) >>> 40);
 		//byte byte4 = (byte)((i & 0x000000FF00000000L) >>> 32);
-		
-		//only using 4 bytes
+
+		//usando só 4 bytes
 		byte byte3 = (byte)((i & 0xFF000000) >>> 24); //0
 		byte byte2 = (byte)((i & 0x00FF0000) >>> 16); //0
 		byte byte1 = (byte)((i & 0x0000FF00) >>> 8 ); //0
 		byte byte0 = (byte)((i & 0x000000FF)       );
-		//{0,0,0,byte0} is equivalent, since all shifts >=8 will be 0
+		//{0,0,0,byte0} é equivalente, já que todas as trocas >=8 serão 0
 		return(new byte[]{byte3,byte2,byte1,byte0});
 	}
-	
-	/*
-	 *Encode an array of bytes into another array of bytes at a supplied offset
-	 *@param image     Array of data representing an image
-	 *@param addition Array of data to add to the supplied image data array
-	 *@param offset      The offset into the image array to add the addition data
-	 *@return Returns data Array of merged image and addition data
+
+	 /*
+	 Encripta um array de bytes dentro de outro array de bytes, num offset fornecido
+	 @param image     Array de dados representando uma imagem
+	 @param addition Array de dados para adicionar ao array de imagem fornecido
+	 @param offset      O offset no array da imagem para adicionar os dados
+	 @return Returns data Array merge da imagem e dados
 	 */
 	private static byte[] encode_text(byte[] image, byte[] addition, int offset)
 	{
-		//check that the data + offset will fit in the image
+		//verifica se dados + offset caberá na imagem
 		if(addition.length + offset > image.length)
 		{
-			throw new IllegalArgumentException("File not long enough!");
+			throw new IllegalArgumentException("Imagem não é grande o suficiente!");
 		}
-		//loop through each addition byte
+		//loop em cada byte adicionado
 		for(int i=0; i<addition.length; ++i)
 		{
-			//loop through the 8 bits of each byte
+			//loop nos 8 bits de cada byte
 			int add = addition[i];
-			for(int bit=7; bit>=0; --bit, ++offset) //ensure the new offset value carries on through both loops
+			for(int bit=7; bit>=0; --bit, ++offset)
 			{
-				//assign an integer to b, shifted by bit spaces AND 1
-				//a single bit of the current byte
 				int b = (add >>> bit) & 1;
-				//assign the bit by taking: [(previous byte value) AND 0xfe] OR bit to add
-				//changes the last bit of the byte in the image to be the bit of addition
 				image[offset] = (byte)((image[offset] & 0xFE) | b );
 			}
 		}
 		return image;
 	}
-    
-	/*
-	 *Retrieves hidden text from an image
-	 *@param image Array of data, representing an image
-	 *@return Array of data which contains the hidden text
+
+	 /*
+	 Recupera o texto escondido na imagem
+	 @param image Array de dados, representando uma imagem
+	 @return Array de dados, que contém o texto escondido
 	 */
 	private static byte[] decode_text(byte[] image)
 	{
 		int length = 0;
 		int offset  = 32;
-		//loop through 32 bytes of data to determine text length
-		for(int i=0; i<32; ++i) //i=24 will also work, as only the 4th byte contains real data
+		//loop nos 32 bytes de dados para determinar length do texto
+		for(int i=0; i<32; ++i)
 		{
 			length = (length << 1) | (image[i] & 1);
 		}
-		
+
 		byte[] result = new byte[length];
-		
-		//loop through each byte of text
+
+		//loop em cada byte do texto
 		for(int b=0; b<result.length; ++b )
 		{
-			//loop through each bit within a byte of text
+			//loop em cada bit dos bytes do texto
 			for(int i=0; i<8; ++i, ++offset)
 			{
-				//assign bit: [(new byte value) << 1] OR [(text byte) AND 1]
 				result[b] = (byte)((result[b] << 1) | (image[offset] & 1));
 
 			}
